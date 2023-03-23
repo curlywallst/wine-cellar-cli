2681fbb777973f382043307e397cac25899336a9
github username <github email address>
2023-03-23 17:11:10 -0400

    add search functionality

--- lib/cli.py
+++ lib/cli.py
@@ -6,16 +6,15 @@ from sqlalchemy.orm import sessionmaker
 
 class CLI:
     def __init__(self, user_input):
-        self.value =  user_input
+        self.grapes = [grape for grape in session.query(Grape)]
+        self.wineries = [winery for winery in session.query(Winery)]
+        self.bottles = [bottle for bottle in session.query(Bottle)]
+        self.name = user_input
         CLI.start(self)
 
     def start(self):
         print(' ')
-        print(f'***** Welcome To The Virtual Wine Keeper {self.value} *****')
-        print(' ')
-        print('Here are your current bottles: ')
-        print(' ')
-        print_bottles()
+        print(f'***** Welcome To The Virtual Wine Keeper {self.name} *****')
         print(' ')
 
         exit = False
@@ -23,76 +22,104 @@ class CLI:
                 choice = input(f'Type "list" to see the wineries, bottles or grapes, type "add" to add a bottle, winery or grape, type "search" to find bottles by winery or grape: ')
                 print(' ') 
                 if choice.lower() == "list":
-                     show_data()
+                     show_data(self)
                 elif choice.lower() == "add":
-                     add_data()
+                     add_data(self)
                 elif choice.lower() == "search":
-                     pass
+                     search_data(self)
         
                 print(' ')
                 user_input = input("Would you like to stop now? (Type Y/N): ")
+                print(' ')
                 if user_input == "Y" or user_input == 'y':
                     exit = True
 
-        printer(self.value)
+        printer(self.name)
 
-def add_data():
-        user_action = input("Type B to add a bottle, G to add a grape or W to add a winery: ")
+def add_data(self):
+    user_action = input("Type B to add a bottle, G to add a grape or W to add a winery: ")
+    print(' ')
+
+    if user_action == "G" or user_action == "g":
+        name = input("Type grape name: ")
+        grape = Grape(name = name)
+
+        session.add(grape)
+        session.commit()
+
+        self.grapes.append(grape)
+
+    elif user_action == "W" or user_action == "w":
+        name = input("Type winery name: ")
+        winery = Winery(name = name)
+
+        session.add(winery)
+        session.commit()
+
+        self.wineries.append(winery)
+
+
+    elif user_action == "B" or "b":
+        print_grapes(self.grapes)
+        print_wineries(self.wineries)
+        print(' ')
+        user_input = input("Is your Grape and Winery in the lists above? (Type Y/N): ")
         print(' ')
 
-        if user_action == "G" or user_action == "g":
-            name = input("Type grape name: ")
-            grape = Grape(name = name)
-
-            session.add(grape)
-            session.commit()
-
-        elif user_action == "W" or user_action == "w":
-            name = input("Type winery name: ")
-            winery = Winery(name = name)
-
-            session.add(winery)
-            session.commit()
-
-        elif user_action == "B" or "b":
-            grapes = [grape for grape in session.query(Grape)]
-            wineries = [winery for winery in session.query(Winery)]
-
-            print_grapes(grapes)
-            print_wineries(wineries)
+        while user_input != "Y" and user_input != "y":
+            add_data(self)
+            print(' ')
+            print_grapes(self.grapes)
+            print_wineries(self.wineries)
             print(' ')
             user_input = input("Is your Grape and Winery in the lists above? (Type Y/N): ")
             print(' ')
-            if user_input == "Y" or user_input == 'y':
-                    
-                    user_grape = input("Type the number of the grape from the list above: ")
-                    user_winery = input("Type the number of the winery from the list above: ")
-                    price = input("How much did you pay for the bottle?: " )
-                    score = input("How would you rate it on a scale of 1-10?: " )
 
-                    bottle = Bottle(
-                            price = price,
-                            score = score,
-                            grape_id = grapes[int(user_grape) - 1].id,
-                            winery_id = wineries[int(user_winery) - 1].id
-                    )
+        make_bottle(self)
 
-                    session.add(bottle)
-                    session.commit()
 
-                    print('Congratulations! You have added the following wine to your Vitual Cellar!')
+def make_bottle(self):
+    user_grape = input("Type the number of the grape from the list above: ")
+    user_winery = input("Type the number of the winery from the list above: ")
+    price = input("How much did you pay for the bottle?: " )
+    score = input("How would you rate it on a scale of 1-10?: " )
 
-                    print_bottle(bottle)
+    bottle = Bottle(
+            price = price,
+            score = score,
+            grape_id = self.grapes[int(user_grape) - 1].id,
+            winery_id = self.wineries[int(user_winery) - 1].id
+    )
 
-def show_data():
-    user_action = input("Type B to list bottles, G to list grapes or W to list wineries: ")
+    session.add(bottle)
+    session.commit()
+
+    self.bottles.append(bottle)
+
+    print('Congratulations! You have added the following wine to your Vitual Cellar!')
+
+    print_bottle(bottle)
+
+def search_data(self):
+    user_action = input("Type G to search bottles by grape or W to search bottles by winery: ")
     print(' ')
     if user_action == "G" or user_action == "g":
-        print_grapes()
+        print_grapes(self.grapes)
+        user_pick = input("Type the number of the grape from the list above to see bottles of that grape: ")
+        print(' ')
+        print_bottles(self.grapes[int(user_pick) - 1].bottles)
     elif user_action == "W" or user_action == "w":
-        print_wineries()
+        print_wineries(self.wineries)
+
+def show_data(self):
+    user_action = input("Type B to list your bottles, G to list grapes or W to list wineries: ")
+    print(' ')
+    if user_action == "G" or user_action == "g":
+        print_grapes(self.grapes)
+    elif user_action == "W" or user_action == "w":
+        print_wineries(self.wineries)
     elif user_action == "B" or user_action == "b":
-        print_bottles()
+        print_bottles(self.bottles)
 
 def print_grapes(grapes):
     print(' ')
@@ -110,8 +137,7 @@ def print_wineries(wineries):
     for index, winery in enumerate(wineries):
         print(f'{index + 1}. {winery.name}')
 
-def print_bottles():
-    bottles = [bottle for bottle in session.query(Bottle)]
+def print_bottles(bottles):
     for index, bottle in enumerate(bottles):
         print_bottle(bottle)
     print(' ')   
